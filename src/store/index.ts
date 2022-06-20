fa24347e484cd2c2847d189dbb4176ecd1112ce6
Cikao <zihaoxtei@outlook.com>
2022-06-20 13:00:02 +0800

    build: 引入Vue-router\Vuex和Element-Plus组件库

---
+++ src/store/index.ts
@@ -0,0 +1,11 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+  state() {
+    return {
+      name: 'cikao'
+    }
+  }
+})
+
+export default store
---
+++ src/views/main/main.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Main</div>
+  <el-button type="success">Success</el-button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style></style>
